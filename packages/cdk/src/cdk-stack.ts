f512dbcae8e9a20a61f9bb212137a95506bff2e5
Kenneth Aasan <k.aasan@sportradar.com>
2020-12-02 16:12:54 -0800

    init

--- packages/cdk/src/cdk-stack.ts
+++ packages/cdk/src/cdk-stack.ts
@@ -27,7 +27,7 @@ export class BeerRadarStack extends cdk.Stack {
     // Run every Monday at 6 AM UTC
     // See https://docs.aws.amazon.com/lambda/latest/dg/tutorial-scheduled-events-schedule-expressions.html
     const rule = new events.Rule(this, 'Rule', {
-      schedule: events.Schedule.expression('cron(0 6 * * MON *)'),
+      schedule: events.Schedule.expression('cron(0 6 ? * MON *)'),
     });
 
     rule.addTarget(new targets.LambdaFunction(lambdaFn));
